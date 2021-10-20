6df1158b2cfff3b2c7b1af2734f22678e332c84f
Xiangyang Ju <xiangyang.ju@gmail.com>
2021-10-20 11:44:17 -0700

    up to filtering

--- Pipelines/TrackML_Example/infer_onnx.py
+++ Pipelines/TrackML_Example/infer_onnx.py
@@ -6,11 +6,11 @@ import onnxruntime
 import torch
 from torch import onnx
 
-from torch2onnx import e_onnx_name
-from torch2onnx import f_onnx_name
-from torch2onnx import g_onnx_name
+from torch2onnx import e_onnx_name, f_onnx_name, g_onnx_name
+from torch2onnx import e_input_name, f_input_name, g_input_name
+from torch2onnx import e_output_name, f_output_name, g_output_name
 
-device = 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device_id = 0
 embedding_dim = 8
 r_val = 1.0
@@ -25,10 +25,10 @@ def create_sessions(provider='CPUExecutionProvider'):
     # sess_options.intra_op_num_threads=16
     sess_options.enable_profiling = False
 
-    models = [ onnxruntime.InferenceSession(onnx_name, sess_options) 
+    models = [ onnxruntime.InferenceSession(onnx_name, sess_options, providers=[provider]) 
         for onnx_name in (e_onnx_name, f_onnx_name, g_onnx_name)]
     
-    [x.set_provider([provider]) for x in models]
+    # [x.set_providers([provider]) for x in models]
 
     return models
 
@@ -69,38 +69,46 @@ def build_edges_faiss(spatial, r_val, knn_val):
     
     return edge_list
 
-build_edges = build_edges_faiss
+build_edges = build_edges_frnn
 
 def run_session_with_iobinding(
-    sess, input_name, output_name,
-    in_data, output_size,
-    element_type=np.float16):
-
-    x_ort_val = onnxruntime.OrtValue.ortvalue_from_numpy(
-        in_data, device_type=device, device_id=device_id)
-
-    batch_size = in_data.shape[0]
-    output_shape = (batch_size, output_size)
-    y_ort_val = onnxruntime.OrtValue.ortvalue_from_shape_and_type(
-        output_shape, element_type, device)
-
+    sess, input_data_map, output_name, output_shape,
+    element_type=np.float32):
 
     # OnnxRuntime will copy the data over to the CUDA device 
     # if 'input' is consumed by nodes on the CUDA device
     io_binding = sess.io_binding()
-    io_binding.bind_input(
-        name=input_name,
-        device_type=x_ort_val.device_name(),
-        device_id=device_id, element_type=element_type, 
-        shape=x_ort_val.shape(), buffer_ptr=x_ort_val.data_ptr())
 
-    io_binding.bind_output(
-        name=output_name,
-        device_type=y_ort_val.device_name(),
-        device_id=device_id, element_type=element_type,
-        shape=y_ort_val.shape(), buffer_ptr=y_ort_val.data_ptr())
+    for key,val in input_data_map.items():
+        x_ort_val = onnxruntime.OrtValue.ortvalue_from_numpy(
+            val, device_type=device, device_id=device_id)
+        io_binding.bind_ortvalue_input(key, x_ort_val)
+
     
+    y_ort_val = onnxruntime.OrtValue.ortvalue_from_shape_and_type(
+        output_shape, element_type, device)
+    io_binding.bind_ortvalue_output(output_name, y_ort_val)
+
+
+    # io_binding.bind_input(
+    #     name=input_name,
+    #     device_type=x_ort_val.device_name(),
+    #     device_id=device_id, element_type=element_type, 
+    #     shape=x_ort_val.shape(), buffer_ptr=x_ort_val.data_ptr())
+
+    # io_binding.bind_output(
+    #     name=output_name,
+    #     device_type=y_ort_val.device_name(),
+    #     device_id=device_id, element_type=element_type,
+    #     shape=y_ort_val.shape(), buffer_ptr=y_ort_val.data_ptr())
+
+    # print(x_ort_val.device_name(), x_ort_val.data_type())
+    
+    
+
+    # io_binding.bind_output('output')
     sess.run_with_iobinding(io_binding)
+    # return io_binding.get_outputs()[0]
     return io_binding.copy_outputs_to_cpu()[0]
 
 
@@ -108,33 +116,43 @@ def run_session_with_iobinding(
 # https://github.com/exatrkx/exatrkx-work/blob/libtorch-frnn/Inference/notebooks/inferenceOnnxFrnnGnn.py
 def inference(in_data):
     # in_data should be [r, phi, z]. Order matters.
-    if device == "gpu":
+    if device == "cuda":
         models = create_sessions('CUDAExecutionProvider')
     else:
         models = create_sessions()
 
     e_sess, f_sess, g_sess = models
-
+    e_input_data = {
+        e_input_name[0]: in_data
+    }
+    batch_size = in_data.shape[0]
     spatial = run_session_with_iobinding(
-        e_sess, "hid_features", "embedding_output1", in_data, output_size=embedding_dim
-    )
+        e_sess, e_input_data, e_output_name[0], 
+        output_shape=(batch_size, embedding_dim))
+
     # <NOTE> if device is gpu, the movement is useless.
     spatial = torch.tensor(spatial).to(device)
 
     e_spatial = build_edges(spatial, r_val=r_val, knn_val=knn_val)
 
+    in_data = torch.tensor(in_data, device=device)
     r_dist = torch.sqrt(in_data[:, 0]**2 + in_data[:, 2]**2)
-    e_spatial = e_spatial[:, (r_dist[e_spatial[0] <= r_dist[e_spatial[1]]])]
+    e_spatial = e_spatial[:, (r_dist[e_spatial[0]] <= r_dist[e_spatial[1]])]
 
     # filtering stage requires splitting the data into batches
     f_batch_size = 800_000
     f_loader = torch.split(e_spatial, f_batch_size, dim=1)
 
+    print("in filtering")
     output_list = []
     for _, sample in enumerate(f_loader):
+        f_input_data = {
+            f_input_name[0]: in_data.cpu().numpy(),
+            f_input_name[1]: sample.cpu().numpy(),
+        }
         output = run_session_with_iobinding(
-            f_sess, "doublets", "filtering_output",
-            (in_data[sample[0]], in_data[sample[1]]), output_size=1)
+            f_sess, f_input_data, f_output_name[0],
+            output_shape=(sample.shape[1], 1))
         output_list.append(output)
 
     output_list = np.concatenate(output_list, axis=None)
@@ -157,4 +175,7 @@ if __name__ == '__main__':
     # add_arg('', help='')
     
     args = parser.parse_args()
-    
+    filename = '/home/xju/ocean/lrt/data/NoPileUp_5K_withTruth_processed/1234'
+    data = torch.load(filename, map_location=device)
+    print(data)
+    inference(data.x.cpu().numpy())
